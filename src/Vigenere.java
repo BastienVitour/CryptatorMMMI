7b703e565ed5aa23f33bacb13d246f20f30be9ac
Fabien Arnal <fab.arnal@free.fr>
2024-12-10 16:58:23 +0100

    FIX: reading the input and replace spaces by nothing - replace chaine by chain

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -13,7 +13,7 @@ public class Vigenere{
         while (true) {
             // Prompting the user to enter a string to be encrypted
             System.out.println("Specify the password to encrypt: ");
-            ch = sc.nextLine().trim(); // Reading the input and removing spaces
+            ch = sc.nextLine().replace(" ", ""); // Reading the input and replace spaces by nothing
 
             // Validate if the input contains only letters in lowercase
             if (VerifyChainKey(ch)) {
@@ -34,7 +34,7 @@ public class Vigenere{
         while (true) {
             // Prompting the user to enter a string to be encryted
             System.out.println("Specify the key to be able to encrypt : ");
-            ch = sc.nextLine().trim(); // Reading the input and removing spaces
+            ch = sc.nextLine().replace(" ", ""); // Reading the input and replace spaces by nothing
 
             // Validate if the input contains only letters in lowercase
             if(VerifyChainKey(ch)) {
@@ -59,12 +59,12 @@ public class Vigenere{
     public static void main(String[] args){
 
         // Calling the entryCh() method to get the string from the user
-        String chaine = EntryChain();
+        String chain = EntryChain();
         // Calling the entryKey() method to specify the key
         String key = EntryKey();
 
         // Displaying the input string back to the user
-        System.out.println("The password to encrypt : " + chaine);
+        System.out.println("The password to encrypt : " + chain);
         // Displaying the key to the user
         System.out.println("The key to be able to encrypt : " + key);
 
