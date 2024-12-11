e8459491c8600f5060c1f4424e0057750a99e22f
Bastien Vitour <bastien.vitour@edu.esiee-it.fr>
2024-12-11 08:54:50 +0100

    Decryption with a Polybius square

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Polybius.Encrypt("lionel"));
+        String input = "notmatching";
+        if(!Polybius.IsValidString(input)) {
+            System.out.println("Veuillez entrer un message valide (uniquement des lettres minuscules)");
+        }
+        else {
+            String aaa = Polybius.Encrypt(input);
+            System.out.println(aaa);
+            String bbb = Polybius.Decrypt(aaa);
+            System.out.println(bbb);
+        }
+
 
     }
 }
